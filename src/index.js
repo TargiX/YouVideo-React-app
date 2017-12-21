dfacd92ec863ccd2703479d79c5cb539f207efbd
Ilya Moskovkin <targix8@gmail.com>
2017-12-21 17:37:28 +0600

    display data from youtube api

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import Key from './keys.js'
+import Key from './keys.js';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = Key;
-console.log(API_KEY)
 
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-   console.log(data);
-})
+class App extends Component {
+   constructor(props) {
+      super(props);
 
-const App = () => {
-   return (
-      <div>
-         <SearchBar />
-      </div>
-   );
+      this.state = {videos: [] };
+
+      YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+         this.setState({ videos });
+      });   
+   }
+
+   render() {
+      return (
+         <div>
+            <SearchBar />
+            <VideoDetail video={this.state.videos[0]}/>
+            <VideoList videos={this.state.videos}/>  
+         </div>
+      );
+   } 
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
